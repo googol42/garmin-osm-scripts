e38839ca940d022a34e2c1878a4d9ee6164b0aa5
Andreas P <andreaspreikschat@posteo.de>
2018-10-10 12:45:19 +0200

    Feature: add new version of build script written in python

---
+++ build_map.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+import subprocess
+import pipes
+import os
+
+
+RAM = 10000
+CORES = 6
+STYLE = 'my-outdoor.TYP'
+LANGUAGE = 'de'
+BASE_DIR = './fzk-mde-garmin/Freizeitkarte-Entwicklung'
+
+
+class Downloader(object):
+    def __init__(self, map_to_build, output_name, build_args=None, needed_region=None, download_osm=True):
+        self.map_to_build = map_to_build
+        self.output_name = output_name
+        self.build_args = []
+        if build_args:
+            self.build_args = build_args
+        self.needed_region = needed_region
+        self.download_osm = download_osm
+        self.start_work()
+
+    def start_work(self):
+        if self.needed_region:
+            run_step('create', self.map_to_build)
+            if self.download_osm:
+                run_step('create', self.needed_region)
+                run_step('fetch_osm', self.needed_region)
+            run_step('extract_osm', self.map_to_build)
+        else:
+            if self.download_osm:
+                run_step('create', self.map_to_build)
+                run_step('fetch_osm', self.map_to_build)
+        run_step('fetch_ele', self.map_to_build)
+        run_step('join', self.map_to_build)
+        run_step('split', self.map_to_build)
+        run_step('build', self.map_to_build, self.build_args)
+        run_step('gmapsupp', self.map_to_build)
+
+        file_name = self.move_map()
+        self.apply_style(file_name)
+
+        self.build_args.append('DEXPLORE')
+        run_step('build', self.map_to_build, *self.build_args)
+        run_step('gmapsupp', self.map_to_build)
+        file_name = self.move_map(True)
+        self.apply_style(file_name)
+        self.log_build()
+
+
+    def move_map(self, is_explore_map=False):
+        name = self.output_name
+        if is_explore_map:
+            name += '-explore'
+        name += '.img'
+        subprocess.call(['mv', f'{BASE_DIR}/install/{self.map_to_build}_{LANGUAGE}/gmapsupp.img', f'output/{name}'])
+        return name
+
+    def apply_style(self, file_name):
+        os.chdir('ReplaceTyp')
+        subprocess.call(['./ReplaceTyp.sh', f'../output/{file_name}', STYLE])
+        os.chdir('..')
+
+    def log_build(self):
+        safe_name = pipes.quote(self.map_to_build)
+        subprocess.call([f'echo -e `date +%d.%m.%Y\ %H:%M` build {safe_name} > updates'], shell=True)
+
+
+def run_step(*args):
+    args = list(filter(None, args))
+    os.chdir(BASE_DIR)
+    print(args)
+    subprocess.call(["perl", f"mt.pl" , f"--language={LANGUAGE}", f"--cores={CORES}", f"--ram={RAM}", *args])
+    os.chdir('../..')
+
+if __name__ == '__main__':
+    ## Alps, DEU+, BW
+    Downloader('Freizeitkarte_ALPS', 'alps', 'DEXTENDEDROUTING', 'Freizeitkarte_EUROPE')
+    Downloader('Freizeitkarte_DEU+', 'deutschland', needed_region='Freizeitkarte_EUROPE', download_osm=False)
+    Downloader('Freizeitkarte_BADEN-WUERTTEMBERG', 'bw', needed_region='Freizeitkarte_EUROPE', download_osm=False)
+
+    ## DEU+ and BW
+    # Downloader('Freizeitkarte_DEU+', 'deutschland', needed_region='Freizeitkarte_EUROPE')
+    # Downloader('Freizeitkarte_BADEN-WUERTTEMBERG', 'bw', needed_region='Freizeitkarte_EUROPE', download_osm=False)
+
+    ## DEU and BW
+    # Downloader('Freizeitkarte_DEU', 'deutschland')
+    # Downloader('Freizeitkarte_BADEN-WUERTTEMBERG', 'bw', needed_region='Freizeitkarte_DEU', download_osm=False)
+
+    ## Alles unabhängig
+    # Downloader('Freizeitkarte_ALPS', 'alps', 'DEXTENDEDROUTING', 'Freizeitkarte_EUROPE')
+    # Downloader('Freizeitkarte_DEU+', 'deutschland', needed_region='Freizeitkarte_EUROPE')
+    # Downloader('Freizeitkarte_DEU', 'deutschland')
+    # Downloader('Freizeitkarte_BADEN-WUERTTEMBERG', 'bw')
+    # Downloader('Freizeitkarte_IRL', 'irland')
+    # Downloader('Freizeitkarte_SAARLAND', 'saarland')
+    # Downloader('Freizeitkarte_CHE', 'ch')
+    # Downloader('Freizeitkarte_AUT', 'at')
+
